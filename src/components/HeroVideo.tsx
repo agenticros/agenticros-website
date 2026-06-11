85c80fa124b75fa411ceb93db91a0c9ff3a990a8
Chris Matthieu <chrismatthieu@Chriss-MBP.lan>
2026-06-11 14:49:17 -0700

    new video

--- src/components/HeroVideo.tsx
+++ src/components/HeroVideo.tsx
@@ -9,7 +9,7 @@ export default function HeroVideo() {
       <div className="relative h-[calc(100vh-4rem)] w-full">
         <iframe
           className="h-full w-full"
-          src="https://www.youtube.com/embed/_fbWhYcPj0M?autoplay=1&mute=1&playsinline=1&loop=1&playlist=_fbWhYcPj0M&rel=0"
+          src="https://www.youtube.com/embed/osSquANrhdQ?autoplay=1&mute=1&playsinline=1&loop=1&playlist=osSquANrhdQ&rel=0"
           title="AgenticROS demo video"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
